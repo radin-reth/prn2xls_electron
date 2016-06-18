ab123aff83fe03bb2d87c493b0ebdc9ecb4a767b
radin-reth <radin.reth@gmail.com>
2016-06-18 16:41:28 +0700

    font KHmerOS

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var util = require('util');
 var htmlFileName = 'pdf.html', 
     pdfFileName = 'page.pdf';
 
-var child = exec('wkhtmltopdf -L 5mm -R 5mm ' + htmlFileName + ' page.pdf', function(err, stdout, stderr) {
+var child = exec('wkhtmltopdf -s A4 -L 15.05mm -R 19.05mm -T 19.05mm -B 19.05mm ' + htmlFileName + ' page.pdf', function(err, stdout, stderr) {
   if(err) { throw err; }
   util.log(stderr);
 });
